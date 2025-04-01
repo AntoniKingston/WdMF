b7bcf325fd4962fc00924bbf62f7e34b3d17eb6a
WujekHussein <qqtrol@gmail.com>
2025-04-01 14:41:26 +0200

    labczy

--- Lab3/crr_model.py
+++ Lab3/crr_model.py
@@ -1,5 +1,6 @@
 import numpy as np
 from abc import ABC, abstractmethod
+import math
 from pyqtgraph.flowchart.library.functions import modeFilter
 
 class Instrument(ABC):
@@ -8,14 +9,35 @@ class Instrument(ABC):
     @abstractmethod
     def price(self, trajectory):
         pass
-class American_call(Instrument):
+class AmericanCall(Instrument):
     def __init__(self, K):
         super().__init__()
         self.K = K
     def price(self, trajectory):
-        call_trajectory = trajectory - self.K
+        call_trajectory = np.array(trajectory) - self.K
+        call_trajectory = call_trajectory[1:]
         call_trajectory[call_trajectory < 0] = 0
         return np.max(call_trajectory)
+
+class EuropeanCall(Instrument):
+    def __init__(self, K):
+        super().__init__()
+        self.K = K
+    def price(self, trajectory):
+        return max(trajectory[-1]-self.K, 0)
+
+class EuropeanPut(Instrument):
+    def __init__(self, K):
+        super().__init__()
+        self.K = K
+    def price(self, trajectory):
+        return max(self.K - trajectory[-1], 0)
+
+class MaximumInstrument(Instrument):
+    def __init__(self):
+        super().__init__()
+    def price(self, trajectory):
+        return max(trajectory)
 
 
 class CRRModel():
@@ -29,17 +51,33 @@ class CRRModel():
         stock_price = [[S0*(u**(j))*(d**(i-j)) for j in range(i+1)] for i in range(0, T+2)]
         self.stock_price = stock_price
     #instrument is a function that takes in T+1 arguments and returns single value
-    def price_instrument(self, instrument):
-        #generating binary sequence
-        rational_prices = [[None for j in range(i + 1)] for i in range(0, self.T + 2)]
+    def price_instrument(self, instrument: Instrument, trajectory=None):
+        if trajectory is None:
+            trajectory = [self.S0]
+        if len(trajectory) == self.T + 1:
+            return instrument.price(trajectory)
+        return (1/(1+self.r))*(self.pstar*self.price_instrument(instrument, trajectory+[trajectory[-1]*self.u]) + (1-self.pstar) * self.price_instrument(instrument, trajectory+[trajectory[-1]*self.d]))
+
+
+
+
 
 
 
 
 
 def main():
-    model = CRRModel(1.1, 0.9, 1.05, 5, 100)
-    print(model.stock_price)
+    u = 1.1
+    d = 0.9
+    r = 0.05
+    T = 5
+    S0 = 100
+    pstar = (1 + r - d) / (u - d)
+    model = CRRModel(u, d, r, T, S0)
+    strike = 150
+    instrument = EuropeanCall(strike)
+    print(model.price_instrument(instrument))
+    print((1+r)**(-T) * sum([math.comb(T,i) * (pstar**i) * ((1-pstar)**(T-i)) * max((S0 * (u**i) * (d**(T-i))) - strike,0) for i in range(T+1)]))
 if __name__ == '__main__':
     main()
 
--- Lab3/test.py
+++
@@ -1,9 +0,0 @@
-from crr_model import *
-def main():
-    binomial_tree = Node.build_recombining_tree(4)
-    print(binomial_tree.left.left.right.val)
-    binomial_tree.left.right.left.val = 2
-    print(binomial_tree.left.left.right.val)
-
-if __name__ == "__main__":
-    main()
